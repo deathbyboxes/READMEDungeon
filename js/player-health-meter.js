8e6ba4d6ec2afd0b14b72d14895ea5d0435f73d4
KevinW90 <kevinjwilliams76@gmail.com>
2020-07-28 11:21:45 -0400

    separated JS map function to another file

--- js/player-health-meter.js
+++ js/player-health-meter.js
@@ -1,3 +1,4 @@
+console.log('phm.js')
 //create a health meter as a component
 let phm = new Component('#phm', {
   data: {
@@ -42,12 +43,4 @@ function dying() {
     rightHalf.style.opacity = 0;
   }
   phm.render(); //re-render every second
-}
-
-// linearly maps value from the range (a..b) to (c..d)
-function mapRange (value, a, b, c, d) {
-  // first map value from (a..b) to (0..1)
-  value = (value - a) / (b - a);
-  // then map it from (0..1) to (c..d) and return it
-  return c + value * (d - c);
 }

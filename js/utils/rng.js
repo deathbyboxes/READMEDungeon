f8f1e3aafecceedc093ae1cf70a2ca0d5e69b58e
Keith Charles <keithc@ouraysportswear.com>
2020-08-06 13:52:52 -0600

    created static Rand class for seeded rng

--- js/utils/rng.js
+++ js/utils/rng.js
@@ -1,1 +1,22 @@
-export const rng = new Math.seedrandom();
+/*
+  ! This class replaces the Math.random() method. 
+  ! Do not use Math.random() anywhere in the app, instead import this module
+  ! and run one of these functions to produce a random number or decimal
+*/
+export static class Rand {
+  constructor(seed) {
+    this.rng = Math.randomseed(seed);
+  }
+
+  static random = (max, min) => {
+    let num = rng();
+    num = max ? Math.floor(num * max) : num;
+    num = min ? num + min             : num;
+    return num;
+  }
+
+  static randomDecimal = (n) => {
+    return this.rng().toFixed(n);
+  }
+
+}

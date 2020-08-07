1f781af668eec26ea2d41d40654b8ee5be677b5a
Keith Charles <keithc@ouraysportswear.com>
2020-08-06 18:47:24 -0600

    created proper rng class w/ methods

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 /*
  * Bootstrap js module
- * only place initilization functions and variables
+ * only place initialization functions and variables
  * within this script. utilize ES6 modules to import
  * and export dependency modules to any new scripts
  * you create.
  */
+
+import Rand from "./utils/rng.js";
+
+// * giving rng a null value will create an near unpredictable seed.
+// * when you've initialized a seed, you cannot 
+Rand.rng = null
--- js/utils/rng.js
+++ js/utils/rng.js
@@ -3,20 +3,45 @@
   ! Do not use Math.random() anywhere in the app, instead import this module
   ! and run one of these functions to produce a random number or decimal
 */
-export static class Rand {
-  constructor(seed) {
-    this.rng = Math.randomseed(seed);
+class Rand {
+  constructor() {
+    this._rng = null;
   }
 
-  static random = (max, min) => {
-    let num = rng();
-    num = max ? Math.floor(num * max) : num;
-    num = min ? num + min             : num;
+  get rng() {
+    return this._rng;
+  }
+
+  set rng(seed) {
+    if (this._rng)
+      console.error("Cannot change random seed after initialization");
+    else
+      this._rng = new Math.seedrandom(seed);
+    return this._rng;
+  }
+
+  /*
+    * returns a random number with a min/max (inclusive, inclusive)
+    * omitting a min and max results in a random float between 0 and 1
+    * @param max - largest number in the range (inclusive)
+    * @param min - smallest number in the range (inclusive)
+  */
+  random = (max, min = 0) => {
+    let num = this._rng();
+    num = max ? Math.floor(num * (max + 1 - min)) + min : num
     return num;
-  }
+  };
 
-  static randomDecimal = (n) => {
-    return this.rng().toFixed(n);
-  }
-
+  /*
+    * returns a random number with desired number of fractional and integer parts
+    * @param frac - how many decimals you want the number to have
+    * @param int - how many whole number digits you want the number to have
+  */
+  randomDecimal = (frac, int = 0) => {
+    return +(this._rng() * Math.pow(10,int)).toFixed(frac)
+  };
 }
+
+let rand = new Rand();
+
+export default rand;

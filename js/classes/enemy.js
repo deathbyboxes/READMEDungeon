4f3bb52ec8fab12bfeae87c31c045c4d62a00597
Keith Charles <keithc@ouraysportswear.com>
2020-08-07 14:42:06 -0600

    added enemy attack timer functonality

--- js/classes/enemy.js
+++ js/classes/enemy.js
@@ -2,7 +2,24 @@ import Character from "./character.js";
 
 export default class Enemy extends Character {
   // TODO: discuss differences enemy class has from generic character class -kc 8/6/2020
-  constructor(name, hp, atk, spd) {
-    super(name, hp, atk, spd);
+  constructor(name, stats) {
+    super(name, stats);
+    this.attackTimer = null;
+  }
+
+  startAttackTimer (p) {
+    let self = this;
+    this.attackTimer = setInterval(function() {
+      self.attack(p)
+    } , 5000 - (this._stats.spd * 500))
+  }
+
+  stopAttackTimer () {
+    clearInterval(this.attackTimer);
+  }
+
+  destroy() {
+    this.stopAttackTimer();
+    super.destroy();
   }
 }

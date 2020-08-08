56dd824173fa32594f5668b224d58edca1eb106a
Keith Charles <keithc@ouraysportswear.com>
2020-08-07 18:54:01 -0600

    added some console logs to view data in console

--- js/classes/character.js
+++ js/classes/character.js
@@ -9,13 +9,14 @@ export default class Character {
 
   damage(pts) {
     this._stats.hp -= pts;
-    if(this._stats.hp <= 0){
+    if (this._stats.hp <= 0) {
       this._stats.hp = 0;
-      console.log(`${this._name} has perished.`)
+      console.log(`${this._name} has perished.`);
       this.destroy();
       return;
     }
-    console.log(`${this._name} has ${this._stats.hp} health left.`)
+    console.log(`${this._name} takes ${pts} damage and has ${this._stats.hp} health left.`);
+    console.log("")
   }
 
   heal(pts) {
@@ -23,11 +24,11 @@ export default class Character {
   }
 
   attack(char) {
-    console.log(`${this._name} attacks ${char._name} with ${this._stats.atk} damage.`)
+    console.log(
+      `${this._name} attacks ${char._name}.`
+    );
     char.damage(this._stats.atk);
   }
 
-  destroy() {
-
-  }
+  destroy() {}
 }

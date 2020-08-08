93e2ce6356dad517b7cc8483cb193fda9ce6cb3f
Keith Charles <keithc@ouraysportswear.com>
2020-08-07 18:54:31 -0600

    created runnable example in app.js

--- js/app.js
+++ js/app.js
@@ -9,16 +9,29 @@
 import Rand from "./utils/rng.js";
 import Player from "./classes/player.js";
 import Enemy from "./classes/enemy.js";
+import Item from "./classes/item.js";
 
 // * giving rng a null value will create an near unpredictable seed.
 // * when you've initialized a seed, you cannot 
 Rand.rng = null
  
-// * test out attacking features of player and enemy by uncommenting the lines below
-// const p = new Player("Keith", {hp: 100, atk: 5, spd: 7, def: 0});
-// const e = new Enemy("Skeleton Soldier", {hp: 20, atk: 3, spd: 3});
-// e.startAttackTimer(p);
+// * below tests out the equipping and unequipping of weapons and armor
+// * just uncomment and run locally. view information in the console logs
+// const p = new Player("Keith", {hp: 100, atk: 2, spd: 2, def: 0});
+// const e = new Enemy("Skeleton Soldier", {hp: 50, atk: 6, spd: 1});
+// const sword = new Item("Sword", "weapon", {atk: 10, spd: 3});
+// const helmet = new Item("Helmet", "armor", {def: 5});
 
+// e.startAttackTimer(p);
 // setInterval(() => {
 //   p.attack(e);
 // }, 5000 - (p._stats.spd * 500));
+
+// setTimeout(() => {
+//   p.addToInv([sword,helmet])
+//   p.equip(sword, "rHand");
+//   setTimeout(() => {
+//     p.equip(helmet, "head");
+//     p.unequip(sword);
+//   },10000);
+// }, 10000);

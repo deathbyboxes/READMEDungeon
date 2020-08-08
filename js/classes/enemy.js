63bbd7274c35f5840669620b28c71de76aa48847
Keith Charles <keithc@ouraysportswear.com>
2020-08-07 18:52:59 -0600

    formatting changes

--- js/classes/enemy.js
+++ js/classes/enemy.js
@@ -4,18 +4,18 @@ export default class Enemy extends Character {
   // TODO: discuss differences enemy class has from generic character class -kc 8/6/2020
   constructor(name, stats) {
     super(name, stats);
-    this.attackTimer = null;
+    this._attackTimer = null;
   }
 
-  startAttackTimer (p) {
+  startAttackTimer(p) {
     let self = this;
-    this.attackTimer = setInterval(function() {
-      self.attack(p)
-    } , 5000 - (this._stats.spd * 500))
+    this._attackTimer = setInterval(function () {
+      self.attack(p);
+    }, 5000 - this._stats.spd * 500);
   }
 
-  stopAttackTimer () {
-    clearInterval(this.attackTimer);
+  stopAttackTimer() {
+    clearInterval(this._attackTimer);
   }
 
   destroy() {

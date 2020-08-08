29168c66f155cfec9ab2cae5a03d36d18367083d
Keith Charles <keithc@ouraysportswear.com>
2020-08-07 18:53:38 -0600

    added some additional methods to player

--- js/classes/player.js
+++ js/classes/player.js
@@ -1,22 +1,27 @@
 import Character from "./character.js";
+import strFormatList from "../utils/strFormatList.js";
 
 const armorSlots = {
   head: null,
   neck: null,
   torso: null,
   back: null,
-  ring: null
-}
+  ring: null,
+};
 
 const weaponSlots = {
   lHand: null,
-  rHand: null
-}
+  rHand: null,
+};
+
+const invLimit = 5;
+
 class Player extends Character {
   constructor(name, stats) {
     super(name, stats);
     this._armor = armorSlots;
     this._weapon = weaponSlots;
+    this._inv = [];
   }
 
   damage(pts) {
@@ -29,26 +34,54 @@ class Player extends Character {
   }
 
   computeStats(item, isEquip) {
-    let multi = isEquip ? 1 : -1
-    for(const stat of Object.keys(item.stats)) {
-      if (this._stats[`${stat}`])
-        this._stats[`${stat}`] += (item.stats[stat] * multi)
+    let multi = isEquip ? 1 : -1;
+    for (const stat of Object.keys(item.stats)) {
+      if (this._stats[stat] !== undefined)
+        this._stats[stat] += item.stats[stat] * multi;
     }
   }
 
-  equip(item, type, slot) {
-    if(!this[`_${type}`][slot])
-      this[`_${type}`][slot] = item
-    computeStats(item, true)
+  usePotion(item) {
+    this._effects.push(...item.effects);
+    this.removeFromInv(item);
   }
 
-  unequip(type, slot) {
-    let item = this[`_${type}`][slot]
-    this[`_${type}`][slot] = null
-    computeStats(item, false)
-    return item;
+  equip(item, slot) {
+    if (!this[`_${item.type}`][slot]) this[`_${item.type}`][slot] = item;
+    this.computeStats(item, true);
+    console.log(`***${this._name} equipped ${item.name}!***`);
+    console.log("");
+    this.removeFromInv(item);
   }
 
+  unequip(item) {
+    for (const [slot, val] of Object.entries(this[`_${item.type}`])) {
+      if (val)
+        if (val.id === item.id) {
+          console.log(`***${this._name} unequipped ${item.name}!***`);
+          console.log("");
+          this[`_${item.type}`][slot] = null;
+          this.computeStats(item, false);
+          return item;
+        }
+    }
+  }
+
+  addToInv(items) {
+    if (this._inv.length + items.length < invLimit) {
+      console.log(`---Added ${items.map((item) => item.name)} to ${this._name}'s inventory!---`);
+      console.log("");
+      this._inv.push(...items);
+    } else {
+      console.error("Not enough room in your inventory.");
+    }
+  }
+
+  removeFromInv(item) {
+    for (let i = 0; i > this._inv.length; i++) {
+      if (this._inv[i].id === item.id) return this._inv.splice(i, 1);
+    }
+  }
 }
 
 export default Player;

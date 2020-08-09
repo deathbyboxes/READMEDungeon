8ad47a1a37a65cc1a6f715f340f7fa1ab703abc7
Keith Charles <keithc@ouraysportswear.com>
2020-08-08 22:50:37 -0600

    created a function to delegate stats of any object

---
+++ js/utils/statDelegator.js
@@ -0,0 +1,19 @@
+import Rand from "./rng.js";
+
+/*
+    * takes an object of stats and a number of points to randomly delegate points to stats. any stat left at 0 gets +1.
+    * @param stat {Object} - the object of stats to delegate points to
+    * @param pts {number} - the amount of points to delegate
+    * return {Object} - new stats with points
+  */
+
+export default function delegateStats(stats, pts) {
+  const keys = Object.keys(stats);
+  while (pts) {
+    stats[keys[Rand.random(keys.length - 1)]]++;
+    pts--;
+  }
+  return stats.map((stat) => {
+    if (stat === 0) return stat + 1;
+  });
+}

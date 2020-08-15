524c438025c54739b148d8b2641ade570236e8e8
Keith C <keith.k.charles@gmail.com>
2020-08-14 18:56:37 -0600

    created new chest class

---
+++ js/classes/generateChest.js
@@ -0,0 +1,52 @@
+import { potions } from "../data/potions.js";
+import { armor } from "../data/armor.js";
+import { weapons } from "../data/weapons.js";
+import buildElement from "../utils/buildElement.js";
+import Rand from "../utils/rng.js";
+
+const chestItems = [
+  { weight: 2, items: potions },
+  { weight: 1, items: armor },
+  { weight: 1, items: weapons },
+];
+
+const chestAmt = [
+  { amt: 1, weight: 4 },
+  { amt: 2, weight: 6 },
+  { amt: 3, weight: 3 },
+  { amt: 4, weight: 2 },
+  { amt: 5, weight: 1 },
+];
+
+class Chest {
+  constructor(contents) {
+    this._name = "Chest";
+    this._icon = "boxes";
+    this._isLocked = false;
+    this._contents = contents;
+    this._elements = {};
+    this._elements["createIcon"] = buildElement(
+      "touch-icon",
+      { class: "icon" },
+      this.getInfo
+    );
+  }
+
+  get getInfo() {
+    return {
+      name: this._name,
+      contents: this._contents,
+      icon: this._icon,
+      isLocked: this._isLocked,
+    };
+  }
+}
+
+export default function generateChest() {
+  const chestSize = Rand.weightedRandom(chestAmt).amt;
+  let contents = [];
+  for (let i = 0; i < chestSize; i++) {
+    contents.push(Rand.weightedRandom(Rand.weightedRandom(chestItems).items));
+  }
+  return new Chest(contents);
+}
--- kevin.js
+++ kevin.js
@@ -1,30 +1,30 @@
 // kevin's test file
 
-import Player from './js/classes/player.js';
-import Enemy from './js/classes/enemy.js';
-import './js/components/touch-icon.js';
-import TouchIcon from './js/components/touch-icon.js';
+// import Player from './js/classes/player.js';
+// import Enemy from './js/classes/generateEnemy.js';
+// import './js/components/touch-icon.js';
+// import TouchIcon from './js/components/touch-icon.js';
 
-// KEVIN TO TEST ICON CLASS
-const Chad = new Player('Chad',{hp:100, atk:5, spd:5, def:0});
-const Delilah = new Enemy('skeleton soldier',{hp:10, atk:3, spd:3, isLocked:false});
-const Marco = new Enemy('skeleton soldier',{hp:10, atk:3, spd:3, isLocked:false});
-const Dante = new Enemy('skeleton soldier',{hp:10, atk:3, spd:3, isLocked:true});
+// // KEVIN TO TEST ICON CLASS
+// const Chad = new Player('Chad',{hp:100, atk:5, spd:5, def:0});
+// const Delilah = new Enemy('skeleton soldier',{hp:10, atk:3, spd:3, isLocked:false});
+// const Marco = new Enemy('skeleton soldier',{hp:10, atk:3, spd:3, isLocked:false});
+// const Dante = new Enemy('skeleton soldier',{hp:10, atk:3, spd:3, isLocked:true});
 
-let iconBar = document.querySelector('#icon-bar');
-iconBar.appendChild(Chad._elements['createIcon']);
-iconBar.appendChild(Delilah._elements['createIcon']);
-iconBar.appendChild(Marco._elements['createIcon']);
-iconBar.appendChild(Dante._elements['createIcon']);
+// let iconBar = document.querySelector('#icon-bar');
+// iconBar.appendChild(Chad._elements['createIcon']);
+// iconBar.appendChild(Delilah._elements['createIcon']);
+// iconBar.appendChild(Marco._elements['createIcon']);
+// iconBar.appendChild(Dante._elements['createIcon']);
 
-//adding menu icons
-let playerMenu = document.querySelector('#player-menu');
-let moveBtn = new TouchIcon();
-moveBtn.setIcon = 'arrows-alt';
-moveBtn.setClass = 'menu-item';
-playerMenu.appendChild(moveBtn);
+// //adding menu icons
+// let playerMenu = document.querySelector('#player-menu');
+// let moveBtn = new TouchIcon();
+// moveBtn.setIcon = 'arrows-alt';
+// moveBtn.setClass = 'menu-item';
+// playerMenu.appendChild(moveBtn);
 
-let potionBtn = new TouchIcon();
-potionBtn.setIcon = 'prescription-bottle';
-potionBtn.setClass = 'menu-item';
-playerMenu.appendChild(potionBtn);
+// let potionBtn = new TouchIcon();
+// potionBtn.setIcon = 'prescription-bottle';
+// potionBtn.setClass = 'menu-item';
+// playerMenu.appendChild(potionBtn);
